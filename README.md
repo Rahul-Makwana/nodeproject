157089452f6c21f97167550296467d9e730680f8
Rahul Makwana <rahulamakwana20@gnu.ac.in>
2024-03-20 10:38:30 +0530

    node project

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# nodeproject
+![Simplicit&eacute; Software](https://platform.simplicite.io/logos/logo250-grey.png)
+* * *
+
+Node.js&reg; demo
+=================
+
+This project is a simple web frontend example using
+the [Simplicit&eacute;&reg; node.js&reg; &amp; brower JavaScript API](https://www.npmjs.com/package/simplicite)
+to connect to a Simplicit&eacute;&reg; demo backend instance from the **server-side**.
+
+Usage
+-----
+
+This project is packaged to deploy on CloudFoundry but can also be used locally.
+
+To run it locally you need to install (or update) the required packages:
+
+```bash
+npm install
+```
+
+Then you can run the application by:
+
+```bash
+npm run start
+```
+
+The following environment variables are required to point to the appropriate Simplicit&eacute;&reg; backend instance:
+
+- `SIMPLICITE_URL`: URL of the instance (e.g. `http://<host>:<port>[/<context>]`)
+- `SIMPLICITE_USERNAME`: user name
+- `SIMPLICITE_PASSWORD`: user password
+
+- the `/` root page displays the demo product catalog
+- the `/user` page displays information about the user
+
+Usage on CloudFoundry
+---------------------
+
+Adjust the `manifest.yml` to your needs:
+
+- The deployment name, host and domain
+- The host, port, root environment variables pointing to the above Simplicit&eacute;&reg; instance
+
+To deploy manually use:
+
+```bash
+cf push
+```
+
+To undeploy manually use:
+
+```bash
+cf delete <deployment name>
+```
+
+License
+=======
+
+Copyright 2014-2021 Simplicit&eacute; Software
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+[](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ app.js
@@ -0,0 +1,78 @@
+/*  ___ _            _ _    _ _
+ * / __(_)_ __  _ __| (_)__(_) |_ ___
+ * \__ \ | '  \| '_ \ | / _| |  _/ -_)
+ * |___/_|_|_|_| .__/_|_\__|_|\__\___|
+ *             |_|
+ * This example is using the Simplicite node.js & browser JavaScript API
+ */
+'use strict';
+
+import simplicite from 'simplicite';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+function headers(res) {
+	res.header('Cache-Control', 'private, no-cache, no-store, no-transform, must-revalidate');
+	res.header('Expires', '-1');
+	res.header('Pragma', 'no-cache');
+}
+
+const app = simplicite.session({
+	url: process.env.SIMPLICITE_URL || 'https://demo.dev.simplicite.io',
+	username: process.env.SIMPLICITE_USERNAME || 'website',
+	password: process.env.SIMPLICITE_PASSWORD || 'simplicite',
+	debug: false
+});
+
+app.info(`Version: ${simplicite.constants.MODULE_VERSION}`);
+app.debug(app.parameters);
+
+try {
+	const user = await app.login();
+	app.debug("Logged in as " + user.login);
+
+	const args = process.argv.slice(2);
+	const serverHost = process.env.VCAP_APP_HOST || args[0] || '0.0.0.0';
+	const serverPort = process.env.VCAP_APP_PORT || parseInt(args[1]) || 3000;
+
+	const server = express();
+	server.disable("x-powered-by");
+	const dir = dirname(fileURLToPath(import.meta.url));
+	server.use(express.static(dir + '/public'));
+	server.set('view engine', 'pug');
+	server.set('views', dir + '/views');
+
+	const product = app.getBusinessObject('DemoProduct');
+
+	server.get('/', async (req, res) => {
+		app.debug('Home page requested');
+		headers(res);
+		try {
+			const list = await product.search(null, { inlineDocuments: [ 'demoPrdPicture' ] });
+			app.debug(list.length + ' products loaded');
+			res.render('index', { version: simplicite.constants.MODULE_VERSION, products: JSON.stringify(list) });
+		} catch (err) {
+			app.error(err);
+			res.render('index', { error: err.message });
+		}
+	});
+
+	server.get('/user', async (req, res) => {
+		app.debug('User page requested');
+		headers(res);
+		try {
+			const grant = await app.getGrant({ inlinePicture: true });
+			app.debug(grant.login + ' loaded');
+			res.render('user', { grant: JSON.stringify(grant) });
+		} catch (err) {
+			app.error(e);
+			res.render('user', { error: err.message });
+		}
+	});
+
+	server.listen(parseInt(serverPort), serverHost);
+	app.info('Server listening on ' + serverHost + ':' + serverPort);
+} catch (err) {
+	app.log(err);
+}
